503af4efcbdc5fae42991197a5c0f94894bcd12d
Devashish Shivamraj Bhatoolaul <56323394+shivambhatoolaul@users.noreply.github.com>
2021-02-02 09:18:50 -0500

    Add course webscraper

---
+++ code/course_webscraper/scrape_courses.py
@@ -0,0 +1,21 @@
+from Program import Program
+
+"""
+by Shivam Bhatoolaul
+"""
+
+if __name__ == "__main__":
+
+    CSC = Program("CSC")
+    #CSC.pretty_print_every_course()
+    #courses = CSC.courses
+    #c1 = courses[2]
+    #print(c1)
+    #print(c1.is_a_prerequisite_for)
+
+    #ANT = Program("ANT")
+    #ANT.pretty_print_every_course()
+    
+    # broken for now:
+    #CCT = Program("CCT")
+    #CCT.pretty_print_every_course()
