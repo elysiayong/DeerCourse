9742fce3bc2f44a308d0f0059d407cd72d8c5a27
Temy <artem.chirkov@mail.utoronto.ca>
2021-02-07 16:54:28 -0500

    Database init and User model

---
+++ code/backend/README.md
@@ -0,0 +1,8 @@
+# Ratings project - Backend
+## Setup
+`pip3 install --user pipenv`  
+`pipenv --install`
+
+## Running
+`pipenv run python main.py`  
+Note: if using pycharm, make sure the project uses the interpreter at `pipenv --venv` and run the code as a module `python -m backend.main` (example configuration can be found on discord)
---
+++ code/backend/main.py
@@ -0,0 +1,38 @@
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+import uvicorn
+from typing import List
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+if __name__ == "__main__":
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=8080, reload=True)
