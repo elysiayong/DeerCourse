af84f6e0aab407e0713fb135d186246b85081eba
Kyle Xiao <kyle.xiao@mail.utoronto.ca>
2021-02-11 19:27:45 -0800

    Change style course element
     - Remove use of Jumbotron
     - Add links to dependent courses

--- code/frontend/src/components/CourseInfo.js
+++ code/frontend/src/components/CourseInfo.js
@@ -1,18 +1,48 @@
 import React from 'react';
-import { Jumbotron, Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 import DATA from './program_data.json';   // Temporary course info "database"
+
+const content = {
+    fontSize: "14pt"
+};
 
 export function CourseInfo(courseName) {
     const courseInfo = tryFetchCourse(courseName);
     if (courseInfo) {
         return (
             <React.Fragment>
-                <Jumbotron fluid>
-                    <Container>
-                        <h1>{courseName}</h1>
-                        <p>{courseInfo['description']}</p>
-                    </Container>
-                </Jumbotron>
+                <Container>
+                    <h1>{courseName}</h1>
+                    <p style={content}>{courseInfo['description']}</p>
+                    <Row>
+                        <Col md>
+                            <h5>Prerequisites</h5>
+                            <p style={content}>N/A</p>
+                        </Col>
+                        <Col md>
+                            <h5>Corequisites</h5>
+                            <p style={content}>N/A</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md>
+                            <h5>Exclusions</h5>
+                            <p style={content}>N/A</p>
+                        </Col>
+                        <Col md>
+                            <h5>Dependent Courses</h5>
+                            <p style={content}>
+                                {courseInfo['prerequisite_for'].map((item, index) => {
+                                    var link = <Link style={content} to={'/course/' + item}>{item}</Link>;
+                                    return (
+                                        <React.Fragment>{link} </React.Fragment>
+                                    );
+                                })}
+                            </p>
+                        </Col>
+                    </Row>
+                </Container>
             </React.Fragment>
         );
     } else return false;
