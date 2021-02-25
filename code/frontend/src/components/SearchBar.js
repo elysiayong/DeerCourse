45f98aa39046abbc78d668f2ae56c31a1ce8b60e
Kyle Xiao <kyle.xiao@mail.utoronto.ca>
2021-02-24 21:21:35 -0500

    DEV-4 Consolidating SearchBar form fields in one Form

--- code/frontend/src/components/SearchBar.js
+++ code/frontend/src/components/SearchBar.js
@@ -1,56 +1,58 @@
 import React from 'react';
 // import { Link, Redirect, useParams  } from "react-router-dom";
-import { Accordion, Button, Card, Form } from "react-bootstrap";
+import { Accordion, Button, Card, Col, Form } from "react-bootstrap";
 
 export const SearchBar = () => (
     <Accordion>
-        <Card>
+        <Form as={Card} bg="light" text="dark">
             <Card.Header>
-                <Form inline>
-                    <Form.Control placeholder="Search for courses..." />
-                    <Form.Text></Form.Text>
-                    <Accordion.Toggle as={Button} eventKey="0">
-                        Filter
-                    </Accordion.Toggle>
-                    <Button type="submit">
-                        Search
-                    </Button>
-                </Form>
+                <Form.Row>
+                    <Form.Group as={Col} xs="auto" style={{ margin: "0" }}>
+                        <Form.Control type="text "placeholder="Search for courses..." />
+                        <Form.Text></Form.Text>
+                    </Form.Group>
+                    <Form.Group style={{ margin: "0" }}>
+                        <Accordion.Toggle as={Button} eventKey="0">
+                            Filter
+                        </Accordion.Toggle>
+                        <Button type="submit">
+                            Search
+                        </Button>
+                    </Form.Group>
+                </Form.Row>
             </Card.Header>
             <Accordion.Collapse eventKey="0">
                 <Card.Body>
-                    ADVANCED SEARCH
-                    <Form inline>
-                        <Form.Group controlId="year">
-                            <Form.Control as="select" size="sm" custom>
-                                <option>Year</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId="session">
-                            <Form.Control as="select" size="sm" custom>
-                                <option>Session</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId="subject">
-                            <Form.Control as="select" size="sm" custom>
-                                <option>Subject Area</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </Form.Control>
-                        </Form.Group>
-                    </Form>
+                    <Card.Title>ADVANCED SEARCH</Card.Title>
+                    <Form.Group controlId="year">
+                        <Form.Control as="select" size="sm" custom>
+                            <option hidden>Year</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="session">
+                        <Form.Control as="select" size="sm" custom>
+                            <option hidden>Session</option>
+                            <option value="f">Fall</option>
+                            <option value="w">Winter</option>
+                            <option value="s">Summer</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="subject">
+                        <Form.Control as="select" size="sm" custom>
+                            <option hidden>Subject Area</option>
+                            {/* This will be dynamic */}
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Form.Control>
+                    </Form.Group>
                 </Card.Body>
             </Accordion.Collapse>
-        </Card>
+        </Form>
     </Accordion>
 )
