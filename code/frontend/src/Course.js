86bd4404ceae0277594b63a4b08959014dea53a9
Kyle Xiao <kyle.xiao@mail.utoronto.ca>
2021-02-26 02:32:08 -0500

    Minor style change to CourseInfo.js

--- code/frontend/src/Course.js
+++ code/frontend/src/Course.js
@@ -1,19 +1,14 @@
-import React from 'react'
-import { CourseInfo } from './components/CourseInfo';
-import { Redirect, useParams } from 'react-router-dom';
-import { Content } from './components/StyledComponents'
+import React from "react";
+import { CourseInfo } from "./components/CourseInfo";
+import { Redirect, useParams } from "react-router-dom";
+import { Content } from "./components/StyledComponents";
 
 export function CoursePage() {
-    let { courseName } = useParams();
-    const courseDisplay = CourseInfo(courseName);
-    if (courseDisplay) {
-        return (
-            <Content>
-                {courseDisplay}
-            </Content>
-        );
-    } else {
-        return <Redirect to="/404" />;
-    }
+  let { courseName } = useParams();
+  const courseDisplay = CourseInfo(courseName);
+  if (courseDisplay) {
+    return <Content>{courseDisplay}</Content>;
+  } else {
+    return <Redirect to="/404" />;
+  }
 }
-
--- code/frontend/src/Home.js
+++ code/frontend/src/Home.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import {SearchBar} from './components/SearchBar';
-
-import sbp from './assets/search-bar-placeholder.png'
-import { ContentCenter } from './components/StyledComponents'
-import utmLogo from './assets/utm-logo.png'
+import React from "react";
+import { SearchBar } from "./components/SearchBar";
+import { ContentCenter } from "./components/StyledComponents";
+import utmLogo from "./assets/utm-logo.png";
 
 export const Home = () => (
   <ContentCenter>
     <h1 class="main-title"> WELCOME TO DEERCOURSE 🦌</h1>
-    <h2 class="main-sub">a Course Review Archive by Students at the University of Toronto Mississauga</h2>
+    <h2 class="main-sub">
+      a Course Review Archive by Students at the University of Toronto
+      Mississauga
+    </h2>
     <br></br>
-    <SearchBar
-      showFilter={false}
-      collapsible={false}
+    <SearchBar showFilter={false} collapsible={false} />
+    <img
+      src={utmLogo}
+      style={{ position: "absolute", bottom: 25, right: 25, width: 150 }}
+      alt="UTM Logo"
     />
-
-    <img src={utmLogo} style={{position: "absolute", bottom: 25, right: 25, width: 150}} alt="UTM Logo"/>
   </ContentCenter>
-)
+);
