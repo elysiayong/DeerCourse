e3f3d0b3e70c63c6a25b1e10449861d645755cda
VyVy Ngo <vyvy.ngo@mail.utoronto.ca>
2021-02-26 01:14:22 -0500

    Add a bookmark page on [DEV-34].

--- code/frontend/src/Bookmarks.js
+++ code/frontend/src/Bookmarks.js
@@ -1,40 +1,70 @@
-import React from 'react'
-import { Container, Accordion, Card, Button } from 'react-bootstrap'
-import { Content } from './components/StyledComponents'
+import styles from './Bookmarks.module.css';
+import React from 'react';
+import { Container, Accordion, Card, Button } from 'react-bootstrap';
+import { Content } from './components/StyledComponents';
+import DATA from './components/program_data.json';   // Temporary course info "database"
 
 export const Bookmarks = () => {
 
+  let courses = createCourseList();
+
+  let bookmarks = courses.map((course, idx) => 
+  {
+    let info = DATA.CSC[course]
     return (
-        <Content>
-            <h1>My Bookmarks</h1>
-            {/* Refactor this part of the code. 
-                Compartamentalize the container into a different file once
-                final design decision has been made.
-            */}
-            <Container>
-                <Accordion defaultActiveKey="0">
-                    <Card>
-                        <Card.Header>
-                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                            Click me!
-                        </Accordion.Toggle>
-                        </Card.Header>
-                        <Accordion.Collapse eventKey="0">
-                        <Card.Body>Hello! I'm the body</Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-                    <Card>
-                        <Card.Header>
-                        <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                            Click me!
-                        </Accordion.Toggle>
-                        </Card.Header>
-                        <Accordion.Collapse eventKey="1">
-                        <Card.Body>Hello! I'm another body</Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-                    </Accordion>
-            </Container>
-        </Content>
+      <Card key={idx} >
+        <Card.Header >
+          <Accordion.Toggle as={Button} className={styles.bookmarkCard} variant="link" eventKey={idx+1}>
+            {course}
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey={idx+1}>
+          <Card.Body className={styles.bookmarkBody}>
+            <h4>Description</h4> 
+            <div className={styles.bookmarkDescription}>{info.description}</div>
+
+            <h4>Exclusions</h4> 
+            <div className={styles.bookmarkDescription}>{info.exclusions}</div>
+
+            <h4>Prerequisites</h4> 
+            <div className={styles.bookmarkDescription}>{info.prerequisites === "" ? 'None' : info.prerequisites}</div>
+
+            <h4>Co-Requisites</h4> 
+            <div className={styles.bookmarkDescription}>{info.co_requisites === "" ? 'None' : info.co_requisites}</div>
+
+            <h4>Prerequisites For</h4> 
+            <div className={styles.bookmarkDescription}>{info.prerequisite_for.length === 0 ? 'None' : info.prerequisite_for.join(", ")}</div>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
     )
+  });
+
+  return (
+    <Content>
+
+      <h1>My Bookmarks</h1>
+
+      <Container >
+        <Accordion>
+          {bookmarks}
+        </Accordion>
+      </Container>
+    </Content>
+  )
+  
+  // Pull information from temporary JSON file
+  // Ignore the spaghetti code pls
+  function createCourseList() {
+
+    let courses = [], course, stop = 0;
+    for (course in DATA.CSC) {
+       courses.push(course);
+       if (stop === 4) {
+         return courses;
+       }
+       stop++;
+     }
+    return courses;
+  }
 }
