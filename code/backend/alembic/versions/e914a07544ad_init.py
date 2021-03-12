e418b2855313a5305aeeed0715f76cf4740021fb
Artish357 <artem.chirkov@mail.utoronto.ca>
2021-03-12 13:08:15 -0500

    [DEV-94] db migrations (#78)
    * [DEV-94] Implement migrations
    
    * [DEV-94] Add files

---
+++ code/backend/alembic/versions/e914a07544ad_init.py
@@ -0,0 +1,24 @@
+"""init
+
+Revision ID: e914a07544ad
+Revises: 
+Create Date: 2021-03-12 12:48:01.406153
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e914a07544ad'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
